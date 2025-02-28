72303ecb86da1370e0158b242cb45e0f00eb9ce1
Kojin Fox <kojinfox@gmail.com>
2025-02-27 16:16:24 -0800

    Create separate styles directory for Pages Router

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,6 +1,6 @@
-import '../app/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
